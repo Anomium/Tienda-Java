622247d5d522d332063b5c141688375c465606b3
Anomium <melvin.martinez.97@gmail.com>
2018-05-01 06:05:16 -0500

    creacion de los modelos Producto y Vendedor

---
+++ src/Model/Producto.java
@@ -0,0 +1,48 @@
+package Model;
+public class Producto {
+    private String nombre;
+    private String ID;
+    private double precio;
+    private int cantidad;
+    
+    public Producto(String nombre, String ID, double precio, int cantidad){
+        this.nombre = nombre;
+        this.ID = ID;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+}
---
+++ src/Model/Vendedor.java
@@ -0,0 +1,48 @@
+package Model;
+public class Vendedor {
+    private String nombre;
+    private String NumDocumento;
+    private String ID;
+    private String Telefono;
+
+    public Vendedor(String nombre, String NumDocumento, String ID, String Telefono) {
+        this.nombre = nombre;
+        this.NumDocumento = NumDocumento;
+        this.ID = ID;
+        this.Telefono = Telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumDocumento() {
+        return NumDocumento;
+    }
+
+    public void setNumDocumento(String NumDocumento) {
+        this.NumDocumento = NumDocumento;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+    
+    
+}
